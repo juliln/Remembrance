09ec95b619bf0fb968aa24479ab5c2cc35002062
Julian Li <jingjl23@uci.edu>
2026-02-27 12:07:04 -0800

    sequencing - opening/gameplay/ending

---
+++ Scripts/SceneManager.gd
@@ -0,0 +1,25 @@
+extends Node
+
+const OPENING = "res://scenes/OpeningCutscene.tscn"
+const WORLD = "res://Areas(Demos)/garden.tscn"
+const ENDING = "res://scenes/Happy_End.tscn"
+
+func _ready():
+	call_deferred("go_to_opening")
+
+func go_to_opening():
+	await get_tree().process_frame
+	await get_tree().process_frame
+	var anim = get_tree().current_scene.get_node("AnimationPlayer")
+	anim.animation_finished.connect(func(_n): 
+		go_to_world()
+	)
+	
+func go_to_world():
+	print("go to world")
+	get_tree().change_scene_to_file(WORLD)
+
+func go_to_ending():
+	get_tree().change_scene_to_file(ENDING)
+	await get_tree().node_added
+	get_tree().current_scene.get_node("AnimationPlayer").animation_finished.connect(func(_n): get_tree().quit())
