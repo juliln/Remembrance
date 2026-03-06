e136031987a6e0adb0650cb9ae8415619352f1c0
Julian Li <jingjl23@uci.edu>
2026-03-05 20:40:31 -0800

    chat update

--- Scripts/Interaction.gd
+++ Scripts/Interaction.gd
@@ -3,8 +3,14 @@ class_name Interactable
 @export var dialogue_lines: Array[String] = []
 
 func interact(_player):
+	print("root", get_tree().root.get_children())
 	var dm = get_tree().root.get_node("World (demo)/UI")
+	# var dm = DialogueManager
 	if dm.is_open:
+		if dm.in_choice:
+			dm.confirm_choice()
+		else:
+			dm.next_line()
 		return
 	if dialogue_lines.size() > 0:
 		dm.show_ui(dialogue_lines)
--- Scripts/SceneManager.gd
+++ Scripts/SceneManager.gd
@@ -11,39 +11,19 @@ func go_to_opening():
 	await get_tree().process_frame
 	await get_tree().process_frame
 	var anim = get_tree().current_scene.get_node("AnimationPlayer")
+	if anim == null:
+		go_to_world()
+		return
 	anim.animation_finished.connect(func(_n): 
 		go_to_world()
 	)
 	
 func go_to_world():
-	print("go to world")
 	get_tree().change_scene_to_file(WORLD)
-
-#func go_to_ending():
-	#get_tree().change_scene_to_file(ENDING)
-	#await get_tree().process_frame
-	#await get_tree().process_frame
-	#var anim = get_tree().current_scene.get_node("End_Scene/AnimatonPlayer")
-	#anim.animation_finished.connect(func(_n): get_tree().quit())
-	##await get_tree().node_added
-	##print("get tree")
-	##get_tree().current_scene.get_node("End_Scene/AnimationPlayer").animation_finished.connect(func(_n): get_tree().quit())
-
-#func go_to_ending(): # new code
-	#
-	#print("go to ending")
-	#get_tree().change_scene_to_file(ENDING)
-	#await get_tree().process_frame
-	#await get_tree().process_frame
-	#var anim = get_tree().current_scene.get_node("End_Scene/AnimationPlayer")
-	#anim.animation_finished.connect(func(_n): get_tree().quit())
 	
 func go_to_ending():
 	get_tree().change_scene_to_file(ENDING)
 	await get_tree().process_frame
 	await get_tree().process_frame
 	get_tree().current_scene.get_node("End_Scene/EndCamera").make_current()
-	print("camera node: ", get_tree().current_scene.get_node("End_Scene/EndCamera"))
-	print("camera viewport: ", get_viewport().get_camera_2d())
-
 	get_tree().current_scene.get_node("End_Scene/AnimationPlayer").animation_finished.connect(func(_n): get_tree().quit())
